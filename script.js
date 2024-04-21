54b52e58880411e8c878600efddc2da3dbf6fc36
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-21 09:05:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function volume_sphere() {
-    //Write your code here
-      let radius = document.getElementById("radius").value;
- if (isNaN(radius) || radius < 0) {
+    let radius = document.getElementById("radius").value;
+
+    if (isNaN(radius) || radius < 0) {
         document.getElementById("volume").value = 'NaN';
     } else {
         let volume = (4/3) * Math.PI * Math.pow(radius, 3);
@@ -9,13 +9,7 @@ function volume_sphere() {
         document.getElementById("volume").value = volume;
     }
 }
-} 
- if (isNaN(radius) || radius < 0) {
-        document.getElementById("volume").value = 'NaN';
-    } else {
-        let volume = (4/3) * Math.PI * Math.pow(radius, 3);
-        volume = volume.toFixed(4);
-        document.getElementById("volume").value = volume;
-    }
-}
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+
+window.onload = function() {
+    document.getElementById('sphereForm').onsubmit = volume_sphere;
+};
